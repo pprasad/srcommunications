c9f41732c98a5d38c8b46f943275ab54054abc28
umprasad <umprasad@cisco.com>
2017-06-11 21:16:50 +0530

    changes on Users.java file

--- srm-rest/src/main/java/com/srm/services/entity/Users.java
+++ srm-rest/src/main/java/com/srm/services/entity/Users.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="USER")
+@Table(name="USER",schema="public")
 public class Users implements Serializable{
 	
 	@Id
