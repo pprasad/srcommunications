0dfc8b913e71377b50a882cbba294b4db15cce48
umprasad <umprasad@cisco.com>
2017-06-11 22:09:54 +0530

    changes on pom.xml & users java file

--- srm-rest/src/main/java/com/srm/services/entity/Users.java
+++ srm-rest/src/main/java/com/srm/services/entity/Users.java
@@ -9,17 +9,17 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="USERS")
-public class Users implements Serializable{
-	
+public class Users implements Serializable {
+
 	@Id
 	@Column(name = "USER_ID")
-	private Integer userId;
+	private Long userId;
 
-	public Integer getUserId() {
+	public Long getUserId() {
 		return userId;
 	}
 
-	public void setUserId(Integer userId) {
+	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
 
